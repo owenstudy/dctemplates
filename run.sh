3b2bb9331988ca8ae174d65d660f42ee2109106d
Guangjun Li <guangjun.li@ebaotech.com>
2019-07-11 10:42:33 +0800

    update for auto kill session

--- run.sh
+++ run.sh
@@ -1,5 +1,6 @@
 # kill the current session,
 # ps -aux|grep python3
+ps -ef |  grep  python3 run.py | grep -v grep | awk '{print $2}' | xargs sudo kill -9
 # kill the session with python3 of key words
 export NLS_LANG='AMERICAN_AMERICA.AL32UTF8'
 nohup python3 run.py&
